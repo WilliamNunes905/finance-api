f3163acac0ecce3e50d6362801dd27e8b813da6b
William Nunes <williamtrybe@gmail.com>
2026-04-22 19:19:00 -0300

    Adjusting the database connection

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -1,14 +1,16 @@
 const mySql = require('mysql2/promise');
 
+require('dotenv').config();
+
 const connection = mySql.createPool({
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE_NAME,
-  waitForConnections: process.env.MYSQL_WAIT_FOR_CONNECTIONS,
-  connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
-  queueLimit: process.env.MYSQL_QUEUE_LIMIT,
+  waitForConnections: process.env.MYSQL_WAIT_FOR_CONNECTIONS === 'true',
+  connectionLimit: Number(process.env.MYSQL_CONNECTION_LIMIT),
+  queueLimit: Number(process.env.MYSQL_QUEUE_LIMIT),
 });
 
 module.exports = connection;
